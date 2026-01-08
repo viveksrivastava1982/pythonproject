80f8d6dd2d7d71430b753394f4538c34291806c3
Vivek Srivastava <vivekjneath16@gmail.com>
2026-01-08 21:43:06 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,10 @@
+a=int(input("please enter first number"))
+b=int(input("please enter second number"))
+c=a+b
+print("addition:",c)
+c=a-b
+print("subtraction:",c)
+c=a*b
+print("multiplication:",c)
+c=a/b
+print("the division:", c)
